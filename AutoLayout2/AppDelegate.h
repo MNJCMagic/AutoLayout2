f17fe76b95ff2a72eb3bf8d8032819038009a16a
Mike Cameron <mike@mikecameron.tv>
2018-04-17 15:45:58 -0400

    Initial Commit

---
+++ AutoLayout2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout2
+//
+//  Created by Mike Cameron on 2018-04-17.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout2
+//
+//  Created by Mike Cameron on 2018-04-17.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
